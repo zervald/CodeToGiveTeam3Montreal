1a88a97ca3b8c40bcb18291ea71ee5a9341ee103
Julien Paquet <paquetjulien@hotmail.com>
2025-11-15 18:38:20 -0500

    fix about button et enleve double navbar+footer

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' }
 ];
