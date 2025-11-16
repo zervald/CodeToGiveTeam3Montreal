f85132b62fc2ff5682fb928502bc2a64e28ec9cb
George Fam <george.fam@famcode.net>
2025-11-16 12:58:56 -0500

    feat: Add initial endpoint for user

---
+++ src/main/java/com/codetogive/codetogitteam3/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.codetogive.codetogitteam3.controller;
+
+import com.codetogive.codetogitteam3.domain.Transaction;
+import com.codetogive.codetogitteam3.domain.User;
+import com.codetogive.codetogitteam3.repository.TransactionRepository;
+import com.codetogive.codetogitteam3.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@Tag(name = "Users", description = "Operations related to users")
+public class UserController {
+    private final TransactionRepository transactionRepository;
+
+    @GetMapping("/{id}/transactions/total")
+    @Operation(summary = "Get total transaction amount for a user")
+    public Double total(@PathVariable Long id) {
+        Double res = transactionRepository.findTotalByUserId(id);
+        return res != null ? res : 0.0;
+    }
+
+}
--- src/main/java/com/codetogive/codetogitteam3/repository/TransactionRepository.java
+++ src/main/java/com/codetogive/codetogitteam3/repository/TransactionRepository.java
@@ -3,10 +3,14 @@ package com.codetogive.codetogitteam3.repository;
 import com.codetogive.codetogitteam3.domain.DonationEvent;
 import com.codetogive.codetogitteam3.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findByUserId(Long userId);
     List<Transaction> findByEventId(Long eventId);
+    @Query("SELECT SUM(t.amount) FROM Transaction t WHERE t.user.id = :userId")
+    Double findTotalByUserId(Long userId);
 }
