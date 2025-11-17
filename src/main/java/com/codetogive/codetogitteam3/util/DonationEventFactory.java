38efc7d5d7b38a5f2120a2ff434ce1be9509c265
Richard Demers <richardd1894@gmail.com>
2025-11-16 20:13:45 -0500

    Add dates to Donation Events (and change few details)

--- src/main/java/com/codetogive/codetogitteam3/util/DonationEventFactory.java
+++ src/main/java/com/codetogive/codetogitteam3/util/DonationEventFactory.java
@@ -2,21 +2,26 @@ package com.codetogive.codetogitteam3.util;
 
 import com.codetogive.codetogitteam3.domain.DonationEvent;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 public class DonationEventFactory {
-    public static List<DonationEvent> build() {
-        return List.of(
-                DonationEvent.builder()
-                        .title("2025 Annual Lilac Gala")
-                        .description("A fundraising event to support community programs and services.")
-                        .goalAmount(10000)
-                        .build(),
-                DonationEvent.builder()
-                        .title("Emergency Shelter Fundraiser")
-                        .description("Raising funds to provide shelter and support for victims of conjugal violence.")
-                        .goalAmount(5000)
-                        .build()
-        );
-    }
+  public static List<DonationEvent> build() {
+    return List.of(
+        DonationEvent.builder()
+            .title("2026 Annual Lilac Gala")
+            .description("A fundraising event to support community programs and services.")
+            .goalAmount(5000)
+            .startDate(LocalDate.of(2025, Month.JANUARY, 1))
+            .endDate(LocalDate.of(2025, Month.JANUARY, 31))
+            .build(),
+        DonationEvent.builder()
+            .title("Emergency Shelter Fundraiser")
+            .description("Raising funds to create a new shelter in Laval and support for victims of conjugal violence.")
+            .goalAmount(10000)
+            .startDate(LocalDate.of(2026, Month.APRIL, 18))
+            .endDate(LocalDate.of(2026, Month.APRIL, 18))
+            .build());
+  }
 }
